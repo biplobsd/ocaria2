d19cead7622d783bee854c398483d2e7a8476927
Biplob Sutradhar <biplobsd11@gmail.com>
2023-01-28 01:10:39 +0600

    ariang native and ariang

--- .ci/dependency.py
+++ .ci/dependency.py
@@ -5,10 +5,23 @@ from pyunpack import Archive
 from urllib.request import urlretrieve
 from enum import Enum
 
+class NamePathTool:
+    def __init__(self, name:str, path:str) -> None:
+        self.name = name
+        self.path = path
+
+class toolTypeUI(Enum):
+    NOTFOUND = NamePathTool('Not', '.')
+    ARIANGWKE = NamePathTool('AriaNgWKE', 'tools/AriaNgWke/AriaNg.exe')
+    ARIANNATIVE = NamePathTool('AriaNg Native', 'tools/AriaNgNative/AriaNg Native.exe')
+    ARIANG = NamePathTool('Ariang', 'tools/AriaNg/index.html')
+
 class toolType(Enum):
     ARIA2 = 1
     ARIANGWKE = 2
     FOUND = 3
+    ARIANGNATIVE = 4
+    ARIANG = 5
 
 
 def findPackageR(id_repo, p_name, tag_name=False, all_=False):
@@ -25,15 +38,21 @@ def findPackageR(id_repo, p_name, tag_name=False, all_=False):
         "not found or maybe api changed!\n Try again with Change packages name")
 
 
-def isNotAvaCheck() -> toolType:
-    if not os.path.exists('aria2/aria2c.exe'):
+def isNotAvaCheck():
+    if not os.path.exists('tools/aria2/aria2c.exe'):
         print("Aria2 not found")
         return toolType.ARIA2
-    elif not os.path.exists('AriaNgWke/AriaNg.exe'):
+    elif not os.path.exists(toolTypeUI.ARIANGWKE.value.path):
         print("AriaNgWke not found")
         return toolType.ARIANGWKE
+    elif not os.path.exists(toolTypeUI.ARIANNATIVE.value.path):
+        print("ARIAN NATIVE not found")
+        return toolType.ARIANGNATIVE
+    elif not os.path.exists(toolTypeUI.ARIANG.value.path):
+        print("ARIANG not found")
+        return toolType.ARIANG
     else:
-        print("Both are found")
+        print("All found")
         return toolType.FOUND
 
 
@@ -50,10 +69,11 @@ def downloadAria2(isx64=True):
     print('aria2 Downloaded')
 
     print('aria2 extracting...')
-    Archive(fileName).extractall(".")
+    Archive(fileName).extractall("tools/")
     print('aria2 extracted')
 
-    os.rename(doc['name'][:-4], 'aria2')
+    os.rename('tools/'+doc['name'][:-4], 'tools/aria2')
+
     os.remove(fileName)
 
 
@@ -67,24 +87,55 @@ def downloadAriangwke(isx64=True):
     print('ariangwke Downloaded')
 
     print('ariangwke extracting...')
-    Archive(fileName).extractall("AriaNgWke", auto_create_dir=True)
+    Archive(fileName).extractall("tools/AriaNgWke", auto_create_dir=True)
     print('ariangwke extracted...')
+
+    os.remove(fileName)
+
+def downloadAriangNative(isx64=True):
+    fileName = 'ariangnative.7z'
+    pType = '64' if isx64 else '32'
+    pUrl = findPackageR('mayswind/AriaNg-Native', f'-Windows-x{pType}.7z')
+    print(pUrl)
+    print('ariang native Downloading...')
+    urlretrieve(pUrl,  fileName)
+    print('ariang native Downloaded')
+
+    print('ariang native extracting...')
+    Archive(fileName).extractall("tools/AriaNgNative", auto_create_dir=True)
+    print('ariang native extracted...')
+
+    os.remove(fileName)
+
+def downloadAriang():
+    fileName = 'ariang.zip'
+    pUrl = findPackageR('mayswind/AriaNg', '-AllInOne.zip')
+    print(pUrl)
+    print('ariang Downloading...')
+    urlretrieve(pUrl,  fileName)
+    print('ariang Downloaded')
+
+    print('ariang extracting...')
+    Archive(fileName).extractall("tools/AriaNg", auto_create_dir=True)
+    print('ariang extracted...')
 
     os.remove(fileName)
 
 
 if __name__ == '__main__':
     isx64 = bool(int(sys.argv[1]))
-
+    uiClient = 0
+    try:
+        uiClient = int(sys.argv[2])
+    except:
+        pass
+    os.makedirs('tools', exist_ok=True)
     if isNotAvaCheck() == toolType.ARIA2:
         downloadAria2(isx64)
     
-    if isNotAvaCheck() == toolType.ARIANGWKE:
+    if uiClient == 0:
         downloadAriangwke(isx64)
-
-    r = isNotAvaCheck()
-    if r == toolType.FOUND:
-        print("Download successful")
-    else:
-        print("Download Unsuccessful")
-        raise Exception('Error while downloading '+ r.value)
+    elif uiClient == 1:
+        downloadAriangNative(isx64)
+    elif uiClient == 2:
+        downloadAriang()
--- ocaria2.py
+++ ocaria2.py
@@ -4,30 +4,46 @@ import subprocess
 import os
 import functools
 import logging
+import sys
 import coloredlogs
 from enum import Enum
 from multiprocessing import Process
 from urllib import request
 from urllib import error
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 logger = logging.getLogger(__name__)
 
 coloredlogs.install(level='DEBUG')
 coloredlogs.install(level='DEBUG', logger=logger)
 
+class NamePathTool:
+    def __init__(self, name:str, path:str) -> None:
+        self.name = name
+        self.path = path
+    
 class toolTypeUI(Enum):
-    NOTFOUND = '-1'
-    ARIANGWKE = 'AriaNg'
-    ARIANNATIVE = 'AriaNg Native'
+    NOTFOUND = NamePathTool('Not', '.')
+    ARIANGWKE = NamePathTool('AriaNgWKE', 'tools/AriaNgWke/AriaNg.exe')
+    ARIANNATIVE = NamePathTool('AriaNg Native', 'tools/AriaNgNative/AriaNg Native.exe')
+    ARIANG = NamePathTool('Ariang', 'tools/AriaNg/index.html')
 
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+    
+        with open(toolTypeUI.ARIANG.value.path, 'rb') as f:
+            self.wfile.write(f.read())
 
 def checkFontend():
-    if os.path.exists('tools/AriaNgWke/AriaNg.exe'):
-        logging.info("AriaNgWke not found")
+    if os.path.exists(toolTypeUI.ARIANGWKE.value.path):
         return toolTypeUI.ARIANGWKE
-    elif os.path.exists('tools/AriaNgNative/AriaNg Native.exe'):
-        logging.info("AriaNgNative not found")
+    elif os.path.exists(toolTypeUI.ARIANNATIVE.value.path):
         return toolTypeUI.ARIANNATIVE
+    elif  os.path.exists(toolTypeUI.ARIANG.value.path):
+        return toolTypeUI.ARIANG
     else:
         return toolTypeUI.NOTFOUND
 
@@ -42,6 +58,14 @@ def run_ariangwke():
     cwd = os.path.join('tools', 'AriaNgWke')
     subprocess.Popen(["AriaNg"], cwd=cwd, shell=True)
 
+def run_ariang(port=6801):
+    logging.info('Serving Ariang...')
+    httpd = HTTPServer(("", port), MyHandler)
+    try:
+        logging.info(f'http://localhost:{httpd.server_port}')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.shutdown()
 
 def run_aria2c():
     OUTPUT_DIR = os.path.join(os.getcwd(), 'downloads')
@@ -54,7 +78,7 @@ def run_aria2c():
         trackers = ''
 
     cmdC = fr'cd {cwd} &&' \
-        r"aria2c --enable-rpc --rpc-listen-port=6800 -D " \
+        r"aria2c --enable-rpc --rpc-allow-origin-all --rpc-listen-port=6800 -D " \
         fr"-d {OUTPUT_DIR} " \
         r"-j 20 " \
         r"-c " \
@@ -78,18 +102,24 @@ def run_aria2c():
 def signal_handler(r, signum, frame):
     if r == toolTypeUI.ARIANNATIVE:
         logging.info('No need to force kill '+r.value)
-        return
-    os.system(fr"taskkill /F /IM AriaNg Native.exe")
-    logging.info(f"{r.value} stopped")
+    elif r == toolTypeUI.ARIANGWKE:
+        os.system(fr"taskkill /F /IM AriaNg.exe")
+        logging.info(f"{r.value} stopped")
 
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
 
+    r = checkFontend()
+    logging.info(r.value.name+" found")
+    if r == toolTypeUI.NOTFOUND:
+        logging.info('Any ui client in tools directory')
+        logging.info('Download ocaria2 from https://github.com/biplobsd/ocaria2/releases')
+        sys.exit(0)
+
     aria2cP = Process(target=run_aria2c)
     aria2cP.start()
 
-    r = checkFontend()
     signal.signal(signal.SIGINT, functools.partial(signal_handler, r))
 
     if r == toolTypeUI.ARIANGWKE:
@@ -100,6 +130,10 @@ if __name__ == "__main__":
         ariangnativeP = Process(target=run_arianative)
         ariangnativeP.start()
         ariangnativeP.join()
+    elif r == toolTypeUI.ARIANG:
+        ariangP = Process(target=run_ariang)
+        ariangP.start()
+        ariangP.join()
     else:
         raise Exception("No fontend ui found")
 
